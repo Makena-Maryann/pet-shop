16e861d56ddae0ca8e3e7e5ee10680ae3e582846
Makena-Maryann <maryann.makena00@gmail.com>
2023-04-02 06:18:13 +0300

    Fix apiResources bug

--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource([
-    'v1/users', UserController::class,
-    'v1/orders', OrderController::class,
-    'v1/payments', PaymentController::class,
-    'v1/order-statuses', OrderStatusController::class,
+Route::apiResources([
+    'v1/users' => UserController::class,
+    'v1/orders' => OrderController::class,
+    'v1/payments' => PaymentController::class,
+    'v1/order-statuses' => OrderStatusController::class,
 ]);
