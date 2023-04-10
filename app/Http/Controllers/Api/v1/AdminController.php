2eb95c3d9f84550c30bbc6226dccc2f325cb8403
Makena-Maryann <maryann.makena00@gmail.com>
2023-04-10 18:51:19 +0300

    Add l5-swagger

--- app/Http/Controllers/Api/v1/AdminController.php
+++ app/Http/Controllers/Api/v1/AdminController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Models\v1\User;
-use App\Filters\UserFilters;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\JsonResponse;
@@ -15,7 +14,106 @@ use App\Http\Requests\UpdateUserRequest;
 class AdminController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *      path="/api/v1/admin/user-listing",
+     *      operationId="getUsersList",
+     *      tags={"Admin"},
+     *      summary="List all users",
+     *      description="Returns all non-admins users",
+     *     @OA\Parameter(
+     *          name="page",
+     *          parameter="page",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="integer"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="limit",
+     *          parameter="limit",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="integer"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="sortBy",
+     *          parameter="sortBy",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="integer"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="desc",
+     *          parameter="desc",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="boolean"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="first_name",
+     *          parameter="first_name",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="email",
+     *          parameter="email",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="phone",
+     *          parameter="phone",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="address",
+     *          parameter="address",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="created_at",
+     *          parameter="created_at",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="string"),
+     *     ),
+     *     @OA\Parameter(
+     *          name="marketing",
+     *          parameter="marketing",
+     *          in="query",
+     *          required=false,
+     *          @OA\Schema(type="boolean"),
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Page Not Found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      ),
+     *      security={
+     *         {"bearerAuth": {}}
+     *    }
+     * )
      */
     public function userListing(Request $request): JsonResponse
     {
@@ -28,37 +126,155 @@ class AdminController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *      path="/api/v1/admin/create",
+     *      operationId="storeAdmin",
+     *      tags={"Admin"},
+     *      summary="Create an Admin account",
+     *      description="Create a new admin account",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/x-www-form-urlencoded",
+     *              @OA\Schema(
+     *                  ref="#/components/schemas/StoreUserRequest"
+     *              )
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Page Not Found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      ),
+     *      security={
+     *         {"bearerAuth": {}}
+     *    }
+     * )
      */
     public function store(StoreUserRequest $request): JsonResponse
     {
         $user = User::create(array_merge($request->validated(), ['is_admin' => true]));
 
-        return response()->json([
-            'success' => 1,
-            'data' => new UserResource($user),
-            'message' => 'User created successfully',
-            'error' => '',
-            'errors' => [],
-            'trace' => '',
-        ], Response::HTTP_CREATED);
+        return \App\Helpers\customApiResponse(true, ['user' => $user]);
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *      path="/api/v1/admin/user-edit/{user}",
+     *      operationId="updateUser",
+     *      tags={"Admin"},
+     *      summary="Edit a User account",
+     *      description="Edits user’s account",
+     *      @OA\Parameter(
+     *          name="user",
+     *          description="User UUID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          ),
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\MediaType(
+     *              mediaType="application/x-www-form-urlencoded",
+     *              @OA\Schema(
+     *                  ref="#/components/schemas/StoreUserRequest"
+     *              )
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Page Not Found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      ),
+     *      security={
+     *         {"bearerAuth": {}}
+     *    }
+     * )
      */
     public function userEdit(UpdateUserRequest $request, User $user)
     {
-        //Edit user’s accounts
-        //Admins accounts cannot be edited
+        $user->update($request->validated());
+
+        return \App\Helpers\customApiResponse(true, ['user' => $user]);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *      path="/api/v1/admin/user-delete/{user}",
+     *      operationId="deleteUser",
+     *      tags={"Admin"},
+     *      summary="Delete a User account",
+     *      description="Deletes a record and returns no content",
+     *      @OA\Parameter(
+     *          name="user",
+     *          description="User UUID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="string"
+     *          ),
+     *      ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="OK",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthorized",
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Page Not Found"
+     *      ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity"
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal Server Error"
+     *      ),
+     *      security={
+     *         {"bearerAuth": {}}
+     *    }
+     * )
      */
     public function userDelete(User $user)
     {
-        //Delete user’s accounts
-        //Admins accounts cannot be deleted
+        $user->delete();
+
+        return \App\Helpers\customApiResponse(true);
     }
 }
